5b39556fcf8331a062bfaccd4fcea17dd45ad096
= <=>
2025-06-29 21:49:20 -0600

    Aca se termino el login

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
 	<link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
-	
+	<div class="contenedor_principal">
+		<div class="contenedor_formulario">
+			<div  class="titulo_empresa" >Variedades 10 y 15</div>
+			<form action="" method="POST" class="form">
+				<label for="codigo" class="label">
+					<input  type="password" class="codigo_input" placeholder="Codigo de Acceso">
+				</label>
+				<input type="submit" value="Acceder" class="boton">
+			</form>
+		</div>
+	</div>
 </body>
 </html>
